49b74dfb98ce7b0a41cd489dd9e5d06b238d4604
Rafael Schouten <rafaelschouten@gmail.com>
2019-02-05 18:16:45 +1100

    cleanup and simplify

--- src/Flatten.jl
+++ src/Flatten.jl
@@ -3,28 +3,19 @@ module Flatten
 using FieldMetadata, Requires 
 import FieldMetadata: @flattenable, @reflattenable, flattenable
 
-export @flattenable, 
-       @reflattenable, 
-       flattenable, 
-       flatten, 
-       reconstruct, 
-       retype, 
-       update!, 
-       metaflatten,
-       fieldnameflatten, 
-       parentflatten, 
-       fieldtypeflatten, 
-       parenttypeflatten 
+export @flattenable, @reflattenable, flattenable, flatten, reconstruct, retype, update!, 
+       metaflatten, fieldnameflatten, parentflatten, fieldtypeflatten, parenttypeflatten 
 
+# Optionally load Unitful and unlittless falttening 
 function __init__()
     @require Unitful="1986cc42-f94f-5a68-af5c-568840ba703d" begin
         using Unitful
-        export ulflatten, 
-               ulreconstruct, 
-               ulupdate!
+        export ulflatten, ulreconstruct, ulupdate!
         include("unitless.jl")
     end
 end
+
+
 
 # Generalised nested struct walker 
 nested(T::Type, expr_builder, expr_combiner, funcname) = 
@@ -55,47 +46,6 @@ flatten(x::Number) = (x,)
 
 
 
-metaflatten_builder(T, fname, funcname) = quote
-    if flattenable($T, Val{$(QuoteNode(fname))})
-        $funcname(getfield(t, $(QuoteNode(fname))), func, $T, Val{$(QuoteNode(fname))})
-    else
-        ()
-    end
-end
-
-metaflatten_inner(T::Type, funcname) = nested(T, metaflatten_builder, default_combiner, funcname)
-
-" Tag flattening. Flattens data attached to a field by methods of a passed in function"
-metaflatten(::Type{Tuple}, t, func) = metaflatten(t, func)
-metaflatten(::Type{V}, t, func) where V <: AbstractVector = [metaflatten(t, func)...]
-
-metaflatten(x::Nothing, func, P, fname) = ()
-metaflatten(x::Number, func, P, fname) = (func(P, fname),)
-metaflatten(xs::NTuple{N,Number}, func, P, fname) where N = map(x -> func(P, fname), xs)
-metaflatten(t, func) = metaflatten(t, func, Nothing, Val{:none})
-@generated metaflatten(t, func, P, fname) = metaflatten_inner(t, :metaflatten)
-
-
-# # Helper functions to get field data with metaflatten
-fieldname_meta(T, ::Type{Val{N}}) where N = N
-fieldtype_meta(T, ::Type{Val{N}}) where N = fieldtype(T, N)
-fieldparent_meta(T, ::Type{Val{N}}) where N = T.name.name
-fieldparenttype_meta(T, ::Type{Val{N}}) where N = T 
-
-fieldnameflatten(T::Type, t) = metaflatten(T, t, fieldname_meta)
-fieldnameflatten(t) = fieldnameflatten(Tuple, t)  
-
-fieldtypeflatten(T::Type, t) = metaflatten(T, t, fieldtype_meta)
-fieldtypeflatten(t) = fieldtypeflatten(Tuple, t) 
-
-parentflatten(T::Type, t) = metaflatten(T, t, fieldparent_meta)
-parentflatten(t) = parentflatten(Tuple, t) 
-
-parenttypeflatten(T::Type, t) = metaflatten(T, t, fieldparenttype_meta)
-parenttypeflatten(t) = parenttypeflatten(Tuple, t)
-
-
-
 reconstruct_builder(T, fname, funcname) = quote
     if flattenable($T, Val{$(QuoteNode(fname))})
         val, n = $funcname(getfield(t, $(QuoteNode(fname))), data, n)
@@ -121,8 +71,7 @@ reconstruct(::Number, data, n) = (data[n],), n + 1
 
 
 
-retype_combiner(T, expressions) = :(($(Expr(:call, :($T.name.wrapper), expressions...)),), n)
-retype_combiner(T::Type{<:Tuple}, expressions) = reconstruct_combiner(T, expressions)
+retype_combiner(T, expressions) = reconstruct_combiner(T.name.wrapper, expressions)
 
 # Reuse the reconstruct expression builder
 retype_inner(::Type{T}, funcname) where T = nested(T, reconstruct_builder, retype_combiner, funcname)
@@ -159,6 +108,50 @@ end
 update!(::Nothing, data, n) = (nothing,), n
 update!(::Number, data, n) = (data[n],), n + 1 
 @generated update!(t::T, data, n) where T = update_inner(T, :update!)
+
+
+
+metaflatten_builder(T, fname, funcname) = quote
+    if flattenable($T, Val{$(QuoteNode(fname))})
+        $funcname(getfield(t, $(QuoteNode(fname))), func, $T, Val{$(QuoteNode(fname))})
+    else
+        ()
+    end
+end
+
+metaflatten_inner(T::Type, funcname) = nested(T, metaflatten_builder, default_combiner, funcname)
+
+" Metadata flattening. Flattens data attached to a field using a passed in function"
+metaflatten(::Type{Tuple}, t, func) = metaflatten(t, func)
+metaflatten(::Type{V}, t, func) where V <: AbstractVector = [metaflatten(t, func)...]
+
+metaflatten(x::Nothing, func, P, fname) = ()
+metaflatten(x::Number, func, P, fname) = (func(P, fname),)
+metaflatten(xs::NTuple{N,Number}, func, P, fname) where N = map(x -> func(P, fname), xs)
+metaflatten(t, func) = metaflatten(t, func, Nothing, Val{:none})
+@generated metaflatten(t, func, P, fname) = metaflatten_inner(t, :metaflatten)
+
+
+# Helper functions to get field data with metaflatten
+fieldname_meta(T, ::Type{Val{N}}) where N = N
+
+fieldnameflatten(T::Type, t) = metaflatten(T, t, fieldname_meta)
+fieldnameflatten(t) = fieldnameflatten(Tuple, t)  
+
+fieldtype_meta(T, ::Type{Val{N}}) where N = fieldtype(T, N)
+
+fieldtypeflatten(T::Type, t) = metaflatten(T, t, fieldtype_meta)
+fieldtypeflatten(t) = fieldtypeflatten(Tuple, t) 
+
+fieldparent_meta(T, ::Type{Val{N}}) where N = T.name.name
+
+parentflatten(T::Type, t) = metaflatten(T, t, fieldparent_meta)
+parentflatten(t) = parentflatten(Tuple, t) 
+
+fieldparenttype_meta(T, ::Type{Val{N}}) where N = T 
+
+parenttypeflatten(T::Type, t) = metaflatten(T, t, fieldparenttype_meta)
+parenttypeflatten(t) = parenttypeflatten(Tuple, t)
 
 
 end # module
