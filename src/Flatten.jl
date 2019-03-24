8de080c8fa6cf0645a71c8ab29569a1831bd2822
Rafael Schouten <rafaelschouten@gmail.com>
2019-03-24 13:25:49 +1100

    standardise actions for types

--- src/Flatten.jl
+++ src/Flatten.jl
@@ -11,7 +11,17 @@ function __init__()
     @require Unitful="1986cc42-f94f-5a68-af5c-568840ba703d" include("unitless.jl")
 end
 
+# Connect types to actions for all flattenable functions
+abstract type FlattenAction end
 
+struct Use <: FlattenAction end
+struct Ignore <: FlattenAction end
+struct Recurse <: FlattenAction end
+
+action(::Number) = Use()
+action(::Nothing) = Ignore()
+action(::AbstractArray) = Ignore()
+action(x) = Recurse()
 
 # Generalised nested struct walker 
 nested(T::Type, expr_builder, expr_combiner, funcname) = 
@@ -22,6 +32,7 @@ nested(T::Type, P::Type, expr_builder, expr_combiner, funcname) =
 default_combiner(T, expressions) = Expr(:tuple, expressions...)
 
 
+# Flatten
 
 flatten_builder(T, fname, funcname) = quote
     if flattenable($T, Val{$(QuoteNode(fname))})
@@ -33,18 +44,23 @@ end
 
 flatten_inner(T, funcname) = nested(T, flatten_builder, default_combiner, funcname)
 
-"Flattening. Flattens a nested type to a Tuple or Vector"
+flatten(::Ignore, x) = ()
+flatten(::Use, x) = (x,) 
+@generated flatten(::Recurse, t) = flatten_inner(t, :flatten)
+
 flatten(::Type{V}, t) where V <: AbstractVector = V([flatten(t)...])
 flatten(::Type{Tuple}, t) = flatten(t)
-flatten(x::Nothing) = ()
-flatten(x::Number) = (x,) 
-@generated flatten(t) = flatten_inner(t, :flatten)
+
+"Flattening. Flattens a nested type to a Tuple or Vector"
+flatten(t) = flatten(action(t), t) 
 
 
+# Reconstruct
 
 reconstruct_builder(T, fname, funcname) = quote
     if flattenable($T, Val{$(QuoteNode(fname))})
-        val, n = $funcname(getfield(t, $(QuoteNode(fname))), data, n)
+        x = getfield(t, $(QuoteNode(fname)))
+        val, n = $funcname(action(x), x, data, n)
         val
     else
         (getfield(t, $(QuoteNode(fname))),)
@@ -56,33 +72,37 @@ reconstruct_combiner(T::Type{<:Tuple}, expressions) = :(($(Expr(:tuple, expressi
 
 reconstruct_inner(::Type{T}, funcname) where T = nested(T, reconstruct_builder, reconstruct_combiner, funcname)
 
-" Reconstruct an object from partial Tuple or Vector data and another object"
-reconstruct(t, data) = reconstruct(t, data, 1)[1][1]
-reconstruct(::Nothing, data, n) = (nothing,), n
-
+reconstruct(::Ignore, x, data, n) = (x,), n
 # Return a value from the data vector/tuple
 # Also increment vector position counter - the returned n + 1 becomes the new n
-reconstruct(::Number, data, n) = (data[n],), n + 1 
-@generated reconstruct(t, data, n) = reconstruct_inner(t, :reconstruct)
+reconstruct(::Use, x, data, n) = (data[n],), n + 1 
+@generated reconstruct(::Recurse, t, data, n) = reconstruct_inner(t, :reconstruct)
+
+" Reconstruct an object from partial Tuple or Vector data and another object"
+reconstruct(t, data) = reconstruct(action(t), t, data, 1)[1][1]
 
 
+# Retype
 
 retype_combiner(T, expressions) = reconstruct_combiner(T.name.wrapper, expressions)
 
 # Reuse the reconstruct expression builder
 retype_inner(::Type{T}, funcname) where T = nested(T, reconstruct_builder, retype_combiner, funcname)
 
+retype(::Ignore, x, data, n) = (x,), n
+retype(::Use, x, data, n) = (data[n],), n + 1 
+@generated retype(::Recurse, t, data, n) = retype_inner(t, :retype)
+
 " Retype an object from partial Tuple or Vector data and another object"
-retype(t, data) = retype(t, data, 1)[1][1]
-retype(::Nothing, data, n) = (nothing,), n
-retype(::Number, data, n) = (data[n],), n + 1 
-@generated retype(t, data, n) = retype_inner(t, :retype)
+retype(t, data) = retype(action(t), t, data, 1)[1][1]
 
 
+# Update
 
 update_builder(T, fname, funcname) = quote
     if flattenable($T, Val{$(QuoteNode(fname))})
-        val, n = $funcname(getfield(t, $(QuoteNode(fname))), data, n)
+        x = getfield(t, $(QuoteNode(fname)))
+        val, n = $funcname(action(x), x, data, n)
         setfield!(t, $(QuoteNode(fname)), val[1]) 
     end
     ()
@@ -96,20 +116,23 @@ update_combiner(T::Type{<:Tuple}, expressions) = reconstruct_combiner(T, express
 
 update_inner(::Type{T}, funcname) where T = nested(T, update_builder, update_combiner, funcname)
 
+update!(::Ignore, x, data, n) = (x,), n
+update!(::Use, x, data, n) = (data[n],), n + 1 
+@generated update!(::Recurse, t::T, data, n) where T = update_inner(T, :update!)
+
 " Update a mutable object with a Tuple or Vector"
 update!(t, data) = begin
-    update!(t, data, 1)
+    update!(action(t), t, data, 1)
     t
 end
-update!(::Nothing, data, n) = (nothing,), n
-update!(::Number, data, n) = (data[n],), n + 1 
-@generated update!(t::T, data, n) where T = update_inner(T, :update!)
 
 
+# Metaflatten
 
 metaflatten_builder(T, fname, funcname) = quote
     if flattenable($T, Val{$(QuoteNode(fname))})
-        $funcname(getfield(t, $(QuoteNode(fname))), func, $T, Val{$(QuoteNode(fname))})
+        x = getfield(t, $(QuoteNode(fname)))
+        $funcname(action(x), x, func, $T, Val{$(QuoteNode(fname))})
     else
         ()
     end
@@ -117,18 +140,20 @@ end
 
 metaflatten_inner(T::Type, funcname) = nested(T, metaflatten_builder, default_combiner, funcname)
 
-" Metadata flattening. Flattens data attached to a field using a passed in function"
 metaflatten(::Type{Tuple}, t, func) = metaflatten(t, func)
 metaflatten(::Type{V}, t, func) where V <: AbstractVector = [metaflatten(t, func)...]
 
-metaflatten(x::Nothing, func, P, fname) = ()
-metaflatten(x::Number, func, P, fname) = (func(P, fname),)
-metaflatten(xs::NTuple{N,Number}, func, P, fname) where N = map(x -> func(P, fname), xs)
-metaflatten(t, func) = metaflatten(t, func, Nothing, Val{:none})
-@generated metaflatten(t, func, P, fname) = metaflatten_inner(t, :metaflatten)
+metaflatten(::Ignore, x, func, P, fname) = ()
+metaflatten(::Use, x, func, P, fname) = (func(P, fname),)
+metaflatten(::Recurse, xs::NTuple{N,Number}, func, P, fname) where N = map(x -> func(P, fname), xs)
+@generated metaflatten(::Recurse, t, func, P, fname) = metaflatten_inner(t, :metaflatten)
+
+" Metadata flattening. Flattens data attached to a field using a passed in function"
+metaflatten(t, func) = metaflatten(action(t), t, func, Nothing, Val{:none})
 
 
 # Helper functions to get field data with metaflatten
+#
 fieldname_meta(T, ::Type{Val{N}}) where N = N
 
 fieldnameflatten(T::Type, t) = metaflatten(T, t, fieldname_meta)
--- src/unitless.jl
+++ src/unitless.jl
@@ -1,28 +1,41 @@
 using .Unitful
 export ulflatten, ulreconstruct, ulupdate!
 
-"Unitless flattening. Flattens a nested type to a Tuple or Vector"
+
+# Unitless flatten
+
+ulflatten(::Ignore, x) = ()
+ulflatten(::Use, x) = (x,) 
+ulflatten(::Use, x::Unitful.Quantity) = (x.val,) 
+@generated ulflatten(::Recurse, t) = flatten_inner(t, :ulflatten)
+
 ulflatten(::Type{V}, t) where V <: AbstractVector = V([ulflatten(t)...])
 ulflatten(::Type{Tuple}, t) = ulflatten(t)
-ulflatten(x::Nothing) = ()
-ulflatten(x::Number) = (x,) 
-ulflatten(x::Unitful.Quantity) = (x.val,) 
-@generated ulflatten(t) = flatten_inner(t, :ulflatten)
 
+"Unitless flattening. Flattens a nested type to a Tuple or Vector"
+ulflatten(t) = ulflatten(action(t), t)
+
+
+# Unitless reconstruct
+
+ulreconstruct(::Ignore, x, data, n) = (x,), n
+ulreconstruct(::Use, x, data, n) = (data[n],), n + 1 
+ulreconstruct(::Use, ::T, data, n) where T <: Unitful.Quantity = (unit(T) * data[n],), n + 1
+@generated ulreconstruct(::Recurse, t, data, n) = reconstruct_inner(t, :ulreconstruct)
 
 " Reconstruct an object from partial Tuple or Vector data and another object"
-ulreconstruct(t, data) = ulreconstruct(t, data, 1)[1][1]
-ulreconstruct(::Nothing, data, n) = (nothing,), n
-ulreconstruct(::Number, data, n) = (data[n],), n + 1 
-ulreconstruct(::T, data, n) where T <: Unitful.Quantity = (unit(T) * data[n],), n + 1
-@generated ulreconstruct(t, data, n) = reconstruct_inner(t, :ulreconstruct)
+ulreconstruct(t, data) = ulreconstruct(action(t), t, data, 1)[1][1]
+
+
+# Unitless update
+
+ulupdate!(::Ignore, x, data, n) = (x,), n
+ulupdate!(::Use, x, data, n) = (data[n],), n + 1 
+ulupdate!(::Use, ::T, data, n) where T <: Unitful.Quantity = (unit(T) * data[n],), n + 1
+@generated ulupdate!(::Recurse, t::T, data, n) where T = update_inner(T, :ulupdate!)
 
 " Update a mutable object with partial Tuple or Vector data"
 ulupdate!(t, data) = begin
-    ulupdate!(t, data, 1)
+    ulupdate!(action(t), t, data, 1)
     t
 end
-ulupdate!(::Nothing, data, n) = (nothing,), n
-ulupdate!(::Number, data, n) = (data[n],), n + 1 
-ulupdate!(::T, data, n) where T <: Unitful.Quantity = (unit(T) * data[n],), n + 1
-@generated ulupdate!(t::T, data, n) where T = update_inner(T, :ulupdate!)
