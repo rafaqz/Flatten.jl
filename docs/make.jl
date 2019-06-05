ea4414745729e4d8ad3f680d9e3f04300cf6e418
Rafael Schouten <rafaelschouten@gmail.com>
2019-06-05 13:00:18 +1000

    fix deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,9 @@
 using Documenter, Flatten
 
 makedocs(
-    checkdocs = :all,
-    modules = [Flatten],
     sitename = "Flatten.jl",
-    format = Documenter.HTML(),
-    highlightsig = true,
-    pages = Any[
-        "Flatten" => "index.md",
-    ]
 )
 
 deploydocs(
     repo = "github.com/rafaqz/Flaten.jl.git",
-    osname = "linux",
-    julia = "1.0",
-    target = "build",
-    deps = nothing,
-    make = nothing
 )
